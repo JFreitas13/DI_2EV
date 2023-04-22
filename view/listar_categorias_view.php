ad78cf78f3de11d9dd1affe42daf9333870c3a37
Joana Freitas <joanafreitas13@gmail.com>
2023-04-22 10:03:36 +0200

    View añadir producto

--- view/listar_categorias_view.php
+++ view/listar_categorias_view.php
@@ -16,7 +16,8 @@
         <tr>
             <td> <?php echo $categoria['id'] ?></td>
             <td> <?php echo $categoria['nombre'] ?></td>
-            <td><a href="/index_editar_categorias.php?id=<?php echo $categoria['id'] ?>">Editar</a></td> <!-- TODO. NO ESTA HECHO -->
+            <td><a href="/index_editar_categorias.php?id=<?php echo $categoria['id'] ?>">Ver Productos</a></td> <!-- TODO. NO ESTA HECHO -->
+            <td><a href="/index_editar_categorias.php?id=<?php echo $categoria['id'] ?>&nombre=<?php echo $categoria['nombre'] ?>">Editar</a></td> <!-- TODO. NO ESTA HECHO -->
             <td><a href="/index_borrar_categorias.php?id=<?php echo $categoria['id'] ?>" onclick="return confirm('¿Estás seguro que quieres eliminar la categoria?'); false">Eliminar</a></td> <!-- TODO: REVISAR PORQUE NO ELIMINA. SE QUEDA EN BLANCO Y NO SALTA MENSAJE DE ERROR -->
             <?php } ?>
         </tr>
--- view/update_categoria_view.php
+++ view/update_categoria_view.php
@@ -28,10 +28,11 @@
 
 <form action="" method="post">
     <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+    <input type="hidden" name="nombre" value="<?php echo $nombre; ?>"/>
     <div>
         <p><strong>ID:</strong> <?php echo $id; ?></p>
         <strong>Nombre: </strong> <input type="text" name="nombre" value="<?php echo $nombre; ?>"/><br/>
-        <input type="submit" name="submit" value="Submit">
+        <input type="submit" name="update" value="Modificar">
     </div>
 </form>
 </body>
